fffe4c6d7a1a8ba22fb492ef2ef889283981c159
Elizaveta Glushchenko <elizaveta.glushchenko@softeq.com>
2023-07-05 18:59:13 +0300

    Commit first version

---
+++ InheritancePractice/InheritancePractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InheritancePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog();
+            Console.WriteLine("Name a dog");
+            dog.Name = Console.ReadLine();
+            Console.WriteLine("Lets feed "+dog.Name);
+            dog.Eat();
+        }
+    }
+}
